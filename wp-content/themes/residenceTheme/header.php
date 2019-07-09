7df70e8f2e5585931d560354f30d6a1fae9d5ae6
talrez <tal@angelsense.com>
2019-07-09 08:19:08 +0300

    Added open sans font

--- wp-content/themes/residenceTheme/header.php
+++ wp-content/themes/residenceTheme/header.php
@@ -17,7 +17,7 @@
     <title><?php wp_title(); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-    <link href="https://fonts.googleapis.com/css?family=Alef:400,700&amp;subset=hebrew" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/style.css?vn='.THEME_VERSION ?>">
 	<?php wp_head(); ?>
