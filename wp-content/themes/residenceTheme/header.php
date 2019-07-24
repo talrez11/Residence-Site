7af11ca11be86d35924df134e9b907a97eb0e03e
talrez <tal@angelsense.com>
2019-07-24 13:39:46 +0300

    Added analyics tracking code

--- wp-content/themes/residenceTheme/header.php
+++ wp-content/themes/residenceTheme/header.php
@@ -24,7 +24,16 @@
         wp_enqueue_style('footer-mobile', get_stylesheet_directory_uri().'/css/footer_mobile.css?vn='.THEME_VERSION, array(), true);
     }
     ?>
-	<?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144501258-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-144501258-1');
+    </script>
+    <?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
